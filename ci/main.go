36cba9ddd596892540bc9103f3128f7dff8d4990
phandox <922425+phandox@users.noreply.github.com>
2023-06-03 08:01:20 +0200

    fix: target env properly in ansible playbook

--- ci/main.go
+++ ci/main.go
@@ -84,7 +84,7 @@ func main() {
 		ansibleExec.Container = ansibleExec.Container.WithMountedDirectory(filepath.Join(ansibleExec.MountPath(), "code"), code, dagger.ContainerWithMountedDirectoryOpts{Owner: ansibleExec.User()}).
 			WithWorkdir(filepath.Join(ansibleExec.MountPath(), "code", "ansible"))
 		// Run Ansible phase
-		ansibleExec.Container.WithExec([]string{filepath.Join(ansibleExec.BinDir(), "ansible-playbook"), "-i", "../dev/postgres-vm/do_hosts.yml", "--extra-vars", "exec_env=" + env, "db.yml"}).Stdout(ctx)
+		ansibleExec.Container.WithExec([]string{filepath.Join(ansibleExec.BinDir(), "ansible-playbook"), "-i", fmt.Sprintf("../%s/postgres-vm/do_hosts.yml", env), "--extra-vars", "exec_env=" + env, "db.yml"}).Stdout(ctx)
 	case "destroy":
 		destroy, err := tgruntExec.WithExec([]string{"run-all", "destroy", "--terragrunt-non-interactive", "--terragrunt-exclude-dir", "volumes/"}).Stdout(ctx)
 		if err != nil {
